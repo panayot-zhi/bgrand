69c25f04c3e9dba16e29c0e992cc2aca112e736c
panayot-zhi <panayot_zhi@abv.bg>
2019-07-01 12:05:12 +0300

    Add models.

---
+++ src/BGRandomGenerator/Models/Bank.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace BGRandomGenerator.Models
+{
+    public class Bank
+    {
+        public string Name { get; set; }
+
+        [JsonProperty("BIC")]
+        public string BussinessIdentifierCode { get; set; }
+
+        [JsonProperty("BAE")]
+        public string BankAddressingEntity { get; set; }
+
+        public Bank(string name, string bussinessIdentifierCode, string bankAddressingEntity)
+        {
+            Name = name;
+            BussinessIdentifierCode = bussinessIdentifierCode;
+            BankAddressingEntity = bankAddressingEntity;
+        }
+    }
+}
---
+++ src/BGRandomGenerator/Models/Region.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BGRandomGenerator.Utility
+{
+    public class Region
+    {
+        public string Name { get; set; }       
+
+        public int MinCode { get; set; }
+
+        public int MaxCode { get; set; }
+
+        public Region(string name, int minCode, int maxCode)
+        {
+            Name = name;            
+            MinCode = minCode;
+            MaxCode = maxCode;
+        }
+    }
+}

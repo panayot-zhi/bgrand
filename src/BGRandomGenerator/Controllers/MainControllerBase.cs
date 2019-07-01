02adfe333a31dad99596e2b75b8efebd6f4b2858
panayot-zhi <panayot_zhi@abv.bg>
2019-07-01 12:05:39 +0300

    Add controllers.

---
+++ src/BGRandomGenerator/Controllers/MainControllerBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Hosting;
+
+namespace BGRandomGenerator.Controllers
+{
+    public abstract class MainControllerBase : ControllerBase
+    {
+        protected readonly IHostingEnvironment Environment;        
+
+        protected MainControllerBase(IHostingEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public ActionResult Ping()
+        {
+            return Ok(DateTime.Now);
+        }
+    }
+}
---
+++ src/BGRandomGenerator/Controllers/RandomController.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BGRandomGenerator.Models;
+using BGRandomGenerator.Utility;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BGRandomGenerator.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]/[action]")]    
+    public class RandomController : MainControllerBase
+    {
+        public RandomController(IHostingEnvironment environment) : base(environment)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult<string> Names(string gender, int? number)
+        {            
+            var rootPath = Environment.ContentRootPath;
+            var constantsPath = System.IO.Path.Combine(rootPath, "Constants");            
+            var reader = new Reader(constantsPath);
+
+            if (!number.HasValue)
+            {
+                number = 1;
+            }
+            else if (number < 1 || number > 3)
+            {
+                return BadRequest($"Invalid number of names supplied: {number}");
+            }
+
+            string[] names;
+            string[] surnames;
+            string[] familyNames;
+
+            if ("F".Equals(gender) || "f".Equals(gender))
+            {
+                names = reader.GetFemaleNames();
+
+                if (number == 1)
+                {
+                    return names.Random();
+                }
+
+                if (number == 2)
+                {
+                    familyNames = reader.GetFemaleFamilyNames();
+                    return $"{names.Random()} {familyNames.Random()}";
+                }
+
+                if (number == 3)
+                {
+                    surnames = reader.GetFemaleSurnames();
+                    familyNames = reader.GetFemaleFamilyNames();
+                    return $"{names.Random()} {surnames.Random()} {familyNames.Random()}";
+                }
+            }
+            else
+            {
+                names = reader.GetMaleNames();
+                
+                if (number == 1)
+                {
+                    return names.Random();
+                }
+
+                surnames = familyNames = reader.GetMaleFamilyNames();
+
+                if (number == 2)
+                {
+
+                    return $"{names.Random()} {familyNames.Random()}";
+                }
+
+                if (number == 3)
+                {
+                    return $"{names.Random()} {surnames.Random()} {familyNames.Random()}";
+                }
+            }
+
+            return BadRequest();
+        }
+
+        [HttpGet]
+        public ActionResult<string> Street()
+        {
+            var rootPath = Environment.ContentRootPath;
+            var constantsPath = System.IO.Path.Combine(rootPath, "Constants");
+            var reader = new Reader(constantsPath);
+            return reader.GetStreets().Random();
+        }
+
+        [HttpGet]
+        public ActionResult<Bank> Bank()
+        {
+            var rootPath = Environment.ContentRootPath;
+            var constantsPath = System.IO.Path.Combine(rootPath, "Constants");
+            var reader = new Reader(constantsPath);
+            return reader.GetBanks().Random();
+        }
+
+        [HttpGet]
+        public ActionResult<string> EGN(DateTime? birthDay, string gender, string region)
+        {
+            int? regionCode = null;
+            var r = Constants.Regions.SingleOrDefault(x => x.Key == region).Value;
+            if (r != null)
+            {
+                regionCode = new Random().Next(r.MinCode, r.MaxCode + 1);
+            }
+
+            return Generator.EGN(birthDay, gender, regionCode);
+        }
+
+        [HttpGet]
+        public ActionResult<string> ENCH(DateTime? birthDay, string gender, string region)
+        {
+            int? regionCode = null;
+            var r = Constants.Regions.SingleOrDefault(x => x.Key == region).Value;
+            if (r != null)
+            {
+                regionCode = new Random().Next(r.MinCode, r.MaxCode + 1);
+            }
+
+            return Generator.ENCH(birthDay, gender, regionCode);
+        }
+
+        [HttpGet]
+        public ActionResult<string> LNCH()
+        {
+            return Generator.LNCH();
+        }
+
+        [HttpGet]
+        public ActionResult<string> BULSTAT(int? length)
+        {            
+            if (!length.HasValue)
+            {
+                length = 9;
+            }
+
+            return Generator.BULSTAT(length.Value);
+        }
+    }
+}
